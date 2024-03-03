e6c7f303fec3e65c85edbf1cba9430c3bde01a04
GameGenesis <gamegenesisdev@gmail.com>
2024-03-02 20:54:28 -0500

    Update database to store message history and context

--- website/models.py
+++ website/models.py
@@ -1,7 +1,12 @@
-from sqlalchemy.sql import func
 from . import db
+from sqlalchemy.ext.mutable import MutableList
+
+pickle_type = MutableList.as_mutable(db.PickleType())
 
 class Template(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     prompt = db.Column(db.String(255))
     html = db.Column(db.Text, unique=True, nullable=False)
+    simplified_message_history = db.Column(pickle_type, nullable=True)
+    full_message_history = db.Column(pickle_type, nullable=True)
+    db.Column(db.ARRAY(db.String(255)))
